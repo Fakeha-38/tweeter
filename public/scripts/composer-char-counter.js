423a91bf751a84a0dcb93abcea96823574a52fae
Fakeha Iftikhar <fakeha.iftikhar@gmail.com>
2024-05-30 18:25:56 -0400

    Added html and css for indivial but old tweets on the timeline of the user

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
   // --- our code goes here ---
   const maxlength = 140;
+  const $counter = $('.counter');
   $('#tweet-text').on("input", function() {
     let currentLength = $(this).val().length;
     let charLeft = maxlength - currentLength;
-    $('.counter').text(charLeft);
+    $counter.text(charLeft);
     if (charLeft >= 0) {
-      $('.counter').removeClass('neg-counter');
+      $counter.removeClass('neg-counter');
     } else if (charLeft < 0) {
-      $('.counter').addClass('neg-counter');
+      $counter.addClass('neg-counter');
     }
     });  
 });
